0e5c51c73907be491212b97daf5f2a60f73f6c2a
Roosevelt <19panta97@gmail.com>
2023-01-20 18:50:51 -0500

    update main

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 from dotenv import load_dotenv
+
 load_dotenv()  # take environment variables from .env.
 
 
-from models import product_attribute
+from models import (
+    loyalty_program,
+    pos_category,
+    pos_config,
+    product_attribute_value,
+    product_attribute,
+    product_category,
+    product_product_link,
+    product_template,
+    res_partner_category,
+    res_partner,
+    stock_picking,
+    stock_warehouse,
+    load_invoices_11,
+)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     product_attribute.download_excel()
+    product_attribute_value.download_excel()
+    product_category.download_excel()
+    pos_category.download_excel()
+    res_partner_category.download_excel()
+    res_partner.download_excel()
+
+    stock_warehouse.create()
